ce130ac2ad227fb7dafc57dfa3baef839e4622fb
rustamsahu <1029rustam9201@gmail.com>
2026-08-27 21:00:23 +0530

    Enable server listening on specified port
    Uncommented the app.listen line and commented out module.exports.

--- pfm/index.js
+++ pfm/index.js
@@ -8,5 +8,5 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"./views"));
 app.use(express.urlencoded({extended:false}));
 app.use("/",router);
-// app.listen(process.env.PORT);
-module.exports=app;
+app.listen(process.env.PORT);
+// module.exports=app;
